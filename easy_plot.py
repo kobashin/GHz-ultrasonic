71ee141e11db686ac1538e2a973c58a1d1fcb949
genuine <shin5884genuine@outlook.com>
2021-09-17 01:13:35 +0900

    add:signal files and edit scripts

--- easy_plot.py
+++ easy_plot.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os, tkinter, tkinter.filedialog, tkinter.messagebox
 
-#show the file selection filedialog
+# show the file selection filedialog
 root = tkinter.Tk()
 root.withdraw()
 fTyp = [('','*')]
 iDir = os.path.abspath(os.path.dirname(__file__))
-#tkinter.messagebox.showinfo('簡易プロットプログラムです','どのフォルダのcsvでグラフを作る？')
+# tkinter.messagebox.showinfo('簡易プロットプログラムです','どのフォルダのcsvでグラフを作る？')
 
-#output the processing file name
+# output the processing file name
 file = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
-#tkinter.messagebox.showinfo('oxプログラム',file)
+# tkinter.messagebox.showinfo('oxプログラム',file)
 
 df = np.loadtxt(file, skiprows=5, delimiter=',', encoding='utf-8')
 
@@ -20,12 +20,12 @@ x = np.arange(rows)
 
 
 # 横軸の単位をsecondにしてグラフを見たいときはこちら↓を使う。
-#plt.plot(df[:,0], df[:,1])
+# plt.plot(df[:,0], df[:,1])
 # 横軸を「1～csvファイルの行数」としてグラフを見たいときはこちら↓を使う。
 plt.plot(x, df[:,1])
 
 
-#plt.vlines(np.arange(24800,26400,200), -0.05, 0.05, color='k', linestyle=':', lw=0.5)
-#plt.fill_between([24800,26400], -0.05, 0.05, color='skyblue')
+# plt.vlines(np.arange(24800,26400,200), -0.05, 0.05, color='k', linestyle=':', lw=0.5)
+# plt.fill_between([24800,26400], -0.05, 0.05, color='skyblue')
 
 plt.show()
--- making_figure_gui.py
+++ making_figure_gui.py
@@ -2,34 +2,34 @@ import os, tkinter, tkinter.filedialog, tkinter.messagebox
 import matplotlib.pyplot as plt
 import numpy as np
 
-#show the file selection filedialog
+# show the file selection filedialog
 root = tkinter.Tk()
 root.withdraw()
 fTyp = [('','*')]
 iDir = os.path.abspath(os.path.dirname(__file__))
-tkinter.messagebox.showinfo('自動イメージ作成プログラム','どのフォルダのcsvでグラフを作るの？')
+tkinter.messagebox.showinfo('自動イメージ作成プログラム','CSVファイルが入っているフォルダを選択する')
 
-#output the processing file name
-#file = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
-#tkinter.messagebox.showinfo('oxプログラム',file)
+# output the processing file name
+# file = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+# tkinter.messagebox.showinfo('oxプログラム',file)
 
-#print(file)
-#print(type(file))
+# print(file)
+# print(type(file))
 
-#output the processing directory name
-dir = tkinter.filedialog.askdirectory(initialdir = iDir)
+# output the processing directory name
+askDirDialog = tkinter.filedialog.askdirectory(initialdir = iDir)
 
-os.chdir(dir)
+os.chdir(askDirDialog)
 plt.figure(figsize=(8,6), dpi=800)
 
-for file in os.listdir(dir):
+for file in os.listdir(askDirDialog):
     if file.count('csv'):
         df = np.loadtxt(file, skiprows=5, delimiter=',', encoding='utf-8')
         rows = len(df[:,0])
         x = np.arange(1, rows + 1)
         plt.plot(x, df[:,1])
-        #plt.vlines(np.arange(24800,26400,200), -0.05, 0.05, color='k', linestyle=':', lw=0.5)
-        #plt.fill_between([24800,26400], -0.05, 0.05, color='skyblue')
+        # plt.vlines(np.arange(24800,26400,200), -0.05, 0.05, color='k', linestyle=':', lw=0.5)
+        # plt.fill_between([24800,26400], -0.05, 0.05, color='skyblue')
         plt.title(file)
         plt.savefig(str(file[:-4])+'.png')
         plt.clf()
